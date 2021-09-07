7721121aa2000983e260c0498194b202e93718c6
LocNguyen2000 <lolkun2000@gmail.com>
2021-09-08 00:50:01 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+
+const port = 4000 || process.env.PORT 
+const app = express()
+
+app.get('/api', (req: Request, res: Response) => {
+  res.status(201).json('Hello World')
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
